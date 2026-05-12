157eacbb0ef4e7e571bf3e34a3550dc9d44045d2
Adrien <admcr019@gmail.com>
2026-05-12 14:46:02 +0200

    feat : acceptation du push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import Navbar from './components/Navbar' // On importe ton fichier
+import Navbar from './components/Navbar'
+import Footer from './components/Footer' // N'oublie pas d'importer ton nouveau footer
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      {/* On appelle la Navbar */}
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      {/* 1. Barre de navigation en haut (ou bas sur mobile) */}
       <Navbar />
+
+      {/* 2. Zone de contenu principal qui pousse le footer vers le bas */}
+      <main className="flex-grow">
+        {/* Ici viendront tes futures pages (Accueil, Recherche, etc.) */}
+        <section className="p-10 text-center">
+          <h1 className="text-3xl font-black text-[#1e2da7]">
+            Bienvenue sur Spark<span className="text-[#f06292]">Up</span>
+          </h1>
+          <p className="mt-4 text-gray-600">Prêt pour votre prochaine expérience collective ?</p>
+        </section>
+      </main>
+
+      {/* 3. Footer avec tes liens réseaux et RGPD */}
+      <Footer />
     </div>
   )
 }
